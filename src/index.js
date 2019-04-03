5c70bb2bebc5a27c88af811ce511a89da083d190
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-03 17:22:35 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Fazer café</Text>
+  </View>
+);
+
+export default App;
