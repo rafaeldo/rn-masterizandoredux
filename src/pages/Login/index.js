f95682896ee89c00b0224385f121ed62e97d7d03
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-04 16:29:32 -0300

    update: Login page now requests user for Github api.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import {
+  Container, Input, Button, ButtonText, Error,
+} from './styles';
 
 export default class Login extends Component {
+  state = {
+    username: '',
+  };
+
+  handleSubmit = async () => {
+    const { username } = this.state;
+
+    try {
+      await api.get(`/users/${username}`);
+    } catch (err) {
+      // SET ERROR
+    }
+  };
+
   render() {
-    return <View />;
+    const { username } = this.state;
+
+    return (
+      <Container>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu usuário"
+          value={username}
+          onChangeText={text => this.setState({ username: text })}
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
   }
 }
